d6c854b10be6cc3ba1bfcd45432dbfe0fa5f2ced
i4wu <i4wu@ucsd.edu>
2014-11-14 22:40:15 -0800

    Open offcampus file and uses for foodlist

--- src/MainGooey.java
+++ src/MainGooey.java
@@ -5,9 +5,17 @@ import java.awt.event.*;
 import javax.swing.*;
 
 import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 import javax.swing.JFrame;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainGooey extends JFrame implements ActionListener {
@@ -15,7 +23,7 @@ public class MainGooey extends JFrame implements ActionListener {
 
     public MainGooey() {
         
-       setTitle("Food Roullete");
+       setTitle("Food Roulette");
        setSize(300, 200);
        setLocationRelativeTo(null);
        setDefaultCloseOperation(EXIT_ON_CLOSE); 
@@ -51,9 +59,39 @@ public class MainGooey extends JFrame implements ActionListener {
 	}
 	
 	private String getRandomFood() {
-		String foodPlaces[] = {"place1", "place2", "place3", "place4", "place5"};
+		//String foodPlaces[] = {"place1", "place2", "place3", "place4", "place5"};
+		
+		ArrayList<String> foodPlaces = new ArrayList<String>();
+
+		String filePath = new File("").getAbsolutePath();
+        System.out.println (filePath);
+
+        BufferedReader reader = null;
+
+		//parse file to ArrayList
+		try {
+		    reader = new BufferedReader(new FileReader(filePath + "/resources/offcampus"));
+		    String text = null;
+
+		    while ((text = reader.readLine()) != null) {
+		    	foodPlaces.add(text);
+		    }
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} finally {
+		    try {
+		        if (reader != null) {
+		            reader.close();
+		        }
+		    } catch (IOException e) {
+		    }
+		}
+
+		//return random foodPlace from arrayList
 		Random randomGenerator = new Random();
-		return foodPlaces[randomGenerator.nextInt(5)];
+		return foodPlaces.get(randomGenerator.nextInt(foodPlaces.size()));
 		
 	}
 }
